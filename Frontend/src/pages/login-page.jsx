8563e48bb5e840d0d9517a76f1bbf628047eccba
Roberto <tierradebotones@gmail.com>
2026-03-24 12:12:32 +0000

    actualizando uris en la auth v2

--- Frontend/src/pages/login-page.jsx
+++ Frontend/src/pages/login-page.jsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3001/api";
+    const apiUrl = import.meta.env.VITE_API_URL || (window.location.hostname === "localhost" ? "http://localhost:3001/api" : "https://clon-rincon-canino-back.onrender.com/api");
 
     if (email === "nazaretlinaresferre198@gmail.com") {
       try {

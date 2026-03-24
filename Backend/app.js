499e0554139628b191491fcd237ff8ad97fd7e03
Roberto <tierradebotones@gmail.com>
2026-03-24 11:50:52 +0000

    preparando cors para producción

--- Backend/app.js
+++ Backend/app.js
@@ -23,7 +23,13 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || process.env.NODE_PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://clon-rincon-canino-front.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
